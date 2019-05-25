85ec7b0e40938fb6698271c507d9be1079ecba23
jiro4989 <jiroron666@gmail.com>
2019-05-25 23:24:37 +0900

    :construction: とりあえず軽く方向性だけ実装

--- src/txtconv4mv.nim
+++ src/txtconv4mv.nim
@@ -2,33 +2,53 @@ const doc = """
 txtconv4mv is a command to convert CSV to RPG Maker MV text data.
 
 Usage:
-  txtconv4mv [options] init
+  txtconv4mv [options] config
   txtconv4mv [options] convert <file>
   txtconv4mv (-h | --help)
   txtconv4mv (-v | --version)
 
 Options:
-  -h --help               Show this screen
-  -v --version            Show version
+  -h --help              Show this screen
+  -v --version           Show version
+  -X --debug             Debug mode ON
+  -L --lang <language>   Message language [default: ja]
 """
+
+import docopt
+import txtconv4mv/[message]
+import logging
 
 const
   version = "v1.0.0"
 
-import docopt
+proc execConfig(args: Table[string, Value]) =
+  debug "call `execConfig`, args = ", args
+  let
+    lang = $args["--lang"]
+    msg = message[lang]
+  debug "msg = ", msg
+  echo msg["start"]
+
+proc execConvert(args: Table[string, Value]) =
+  debug "call `execConvert`, args = ", args
+  discard
 
 when isMainModule:
   let args = docopt(doc, version = version)
+  if args["--debug"]:
+    var logger = newConsoleLogger(lvlAll, verboseFmtStr)
+    addHandler(logger)
+  
+  debug "args = ", args
 
-when false:
-  if args["crop"]:
-    execCrop(args)
+  if args["config"]:
+    execConfig(args)
     quit 0
 
-  if args["paste"]:
-    execPaste(args)
+  if args["convert"]:
+    execConvert(args)
     quit 0
   
-  logErr "illegal options"
   stderr.writeLine doc
   quit 1
+  
---
+++ src/txtconv4mv/message.nim
@@ -0,0 +1,27 @@
+import tables
+
+const
+  message* = {
+    "ja": {
+      "start": "これよりツールの設定を行います (Ctrl + Cで処理を中断)",
+      "projectDir": "RPGツクールMVのゲームのプロジェクトフォルダを指定してください",
+      "wrappingBrackets": "文章を括弧で括りますか？",
+      "wrappingBrackets2": "どの括弧を使用しますか？",
+      "indenting": "括弧でくくる際は高さを揃えますか？",
+      "wordWrap": "入力のテキストが表示幅を超過する際は折り返しますか？",
+      "wordWrap2": "何文字で折り返しますか？ [デフォルト:NN]",
+      "finish": "入力した設定を表示します",
+      "finish2": "以上の設定でよろしいですか？",
+    }.toTable,
+    "en": {
+      "start": "これよりツールの設定を行います (Ctrl + Cで処理を中断)",
+      "projectDir": "RPGツクールMVのゲームのプロジェクトフォルダを指定してください",
+      "wrappingBrackets": "文章を括弧で括りますか？",
+      "wrappingBrackets2": "どの括弧を使用しますか？",
+      "indenting": "括弧でくくる際は高さを揃えますか？",
+      "wordWrap": "入力のテキストが表示幅を超過する際は折り返しますか？",
+      "wordWrap2": "何文字で折り返しますか？ [デフォルト:NN]",
+      "finish": "入力した設定を表示します",
+      "finish2": "以上の設定でよろしいですか？",
+    }.toTable,
+  }.toTable
