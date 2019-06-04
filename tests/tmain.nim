c08bd492a3e2df000cf76414da602bad3b84d5ce
jiro4989 <jiroron666@gmail.com>
2019-06-04 17:32:31 +0900

    :fire: Goで作ることにした

--- tests/tmain.nim
+++
@@ -1,8 +0,0 @@
-import unittest
-import txtconv4mv/textevent
-
-suite "main":
-  test "normal":
-    var textevents = readTextEventFile("examples/text1.csv")
-    for ev in textevents:
-      echo ev[]
