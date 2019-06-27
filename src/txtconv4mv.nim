bdaadca2f388811261614f921fb062be5cea7397
jiro4989 <jiroron666@gmail.com>
2019-06-27 19:04:15 +0900

    :construction: 一旦ファイルを生成できるようにだけした

--- src/txtconv4mv.nim
+++ src/txtconv4mv.nim
@@ -1,5 +1,11 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
 
+import txtconv4mv/[sentence, project]
+import json
+
 when isMainModule:
   echo("Hello, World!")
+  let ss = readSentenceFile("examples/text1.csv")
+  let obj = newMapObject(ss, ["<<", ">>"], 80, true, ["「"," 」"])
+  writeFile("Map001.json", obj.pretty)
