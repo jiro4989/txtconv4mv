cb1c6ac91e003574a22290e3860f100fd8717256
jiro4989 <jiroron666@gmail.com>
2019-06-27 17:39:02 +0900

    :white_check_mark: format procのテストコード追加した

--- tests/tsentence.nim
+++ tests/tsentence.nim
@@ -47,5 +47,32 @@ suite "proc wrapEAW":
     check "あいうえ\nさしすせ".wrapEAW(6, true) == @["あいう", "えさし", "すせ"]
 
 suite "proc format":
-  test "Normal":
-    discard
+  test "Empty str":
+    var empty: seq[string]
+    check Sentence(actorName: "", text: "")
+          .format(["", ""], 6, false, ["", ""]) == empty
+  test "Option none":
+    check Sentence(actorName: "", text: "123456")
+          .format(["", ""], 6, false, ["", ""]) == @["123456"]
+  test "Actor":
+    check Sentence(actorName: "Test", text: "123456")
+          .format(["", ""], 6, false, ["", ""]) == @["Test", "123456"]
+  test "Actor, actorBrackets":
+    check Sentence(actorName: "Test", text: "123456")
+          .format(["[", "]"], 6, false, ["", ""]) == @["[Test]", "123456"]
+  test "Actor, actorBrackets, wrap":
+    check Sentence(actorName: "Test", text: "1234567")
+          .format(["[", "]"], 6, false, ["", ""]) == @["[Test]", "123456", "7"]
+  test "Actor, actorBrackets, wrap, useJoin":
+    check Sentence(actorName: "Test", text: "1234567")
+          .format(["[", "]"], 6, true, ["", ""]) == @["[Test]", "123456", "7"]
+    check Sentence(actorName: "Test", text: "1234567\nabcdef")
+          .format(["[", "]"], 6, true, ["", ""]) == @["[Test]", "123456", "7abcde", "f"]
+  test "Actor, actorBrackets, wrap, useJoin, textBrackets":
+    check Sentence(actorName: "Test", text: "1234567\nabcdef")
+          .format(["[", "]"], 6, true, ["<", ">"]) == @["[Test]", "<12345", " 67abc", " def>"]
+    check Sentence(actorName: "Test", text: "1234567\nabcdef")
+          .format(["[", "]"], 6, true, ["「", "」"]) == @["[Test]", "「1234", "  567a", "  bcde", "  f」"]
+  test "Actor, actorBrackets, wrap, useJoin, textBrackets, lastWrap":
+    check Sentence(actorName: "Test", text: "1234567\nabcde")
+          .format(["[", "]"], 6, true, ["「", "」"]) == @["[Test]", "「1234", "  567a", "  bcde", "  」"]
